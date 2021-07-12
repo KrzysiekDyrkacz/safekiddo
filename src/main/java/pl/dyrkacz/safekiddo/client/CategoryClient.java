4119e1ca9bca738525641a0d7121f602b4007bf6
Krzysiek <k.dyr89@gmail.com>
2021-07-12 19:24:01 +0200

    Db heroku

--- src/main/java/pl/dyrkacz/safekiddo/client/CategoryClient.java
+++ src/main/java/pl/dyrkacz/safekiddo/client/CategoryClient.java
@@ -25,8 +25,8 @@ import java.util.stream.IntStream;
 @Service
 public class CategoryClient {
 
-    @Value("${token}")
-    String token;
+//    @Value("${token}")
+//    String token;
 
     public List<String> getData(String siteName) throws IOException {
 //        URL url = new URL("https://www.klazify.com/api/categorize?url=https://" + siteName);
@@ -45,7 +45,7 @@ public class CategoryClient {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
         Map map = new HashMap<String, String>();
         map.put("Content-Type", "application/json");
-        map.put("Authorization", "Bearer " + token);
+        map.put("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTMwZjE0OTJkYmI4MDdjMGY2YzAwOWIwM2E0MWY5YzJkZDQ0MTU2OWU5NDIyMjQ1ZDE3ZjM5M2RmNGU1MmUwZmY3YzYzZjllNWQ0ZDM4MDgiLCJpYXQiOjE2MjU2ODY3NDQsIm5iZiI6MTYyNTY4Njc0NCwiZXhwIjoxNjU3MjIyNzQ0LCJzdWIiOiIyOTQ0Iiwic2NvcGVzIjpbXX0.qdGt0P5OMAtSbYNAI2Wg6kGUqWCXq29d9O7j8pTAnZciGkGmGT9pBbv401--t7Cmh1kprfcps5YyMlvib7G2CQ");
 
         headers.setAll(map);
 
